0bbc014d25ad514eec45441cfd9a38564a35b948
Mirza Abazovic <mirza.abazovic@gmail.com>
2020-05-01 16:12:14 +0200

    persson service

--- spring-aop/src/main/java/ba/programiraj/spring/aop/AopApplication.java
+++ spring-aop/src/main/java/ba/programiraj/spring/aop/AopApplication.java
@@ -1,13 +1,33 @@
 package ba.programiraj.spring.aop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopApplication {
+    private static final Logger log = LoggerFactory.getLogger(AopApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(AopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AopApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            final PersonService personService = ctx.getBean(PersonService.class);
+            final String personId = "XYY";
+            log.info("Is person with id {} live", personId);
+            final boolean isAlive = personService.isPersonAlive(personId);
+            String liveDead = isAlive ? "live" : "dead";
+            log.info("Person with {} is {}", personId, liveDead);
+
+
+        };
+    }
 
 }
--- spring-aop/src/main/java/ba/programiraj/spring/aop/PersonService.java
+++ spring-aop/src/main/java/ba/programiraj/spring/aop/PersonService.java
@@ -1,4 +1,17 @@
 package ba.programiraj.spring.aop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonService {
+    private static final Logger log = LoggerFactory.getLogger(PersonService.class);
+    public boolean isPersonAlive(String personId) {
+        log.info("Start method isPersonAlive");
+        log.info("personId = " + personId);
+        final boolean isAlive = personId.hashCode() % 2 == 0;
+        return isAlive;
+    }
+
 }
